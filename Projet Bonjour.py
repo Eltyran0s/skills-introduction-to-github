a511bfb2e6282050d88b46128d7bb7022b89cbe5
Eltyran0s <knilknil@hotmail.fr>
2025-05-28 15:49:44 +0200

    Add files via upload
    Faire un fichier python qui affiche "Hello world" dans le terminal

---
+++ Projet Bonjour.py
@@ -0,0 +1,16 @@
+# Fonction "Hello world"
+
+# def Hello():
+#     print("Dites Bonjour!")
+#     Bonjour= input(Bonjour)
+#     if "Bonjour":                #J'ai voulu faire un truc plus compliqué mais j'ai laissé tombé
+#     print("Hello world")
+#     else : 
+#     print("Dites Bonjour !")
+
+# Hello()
+
+def Hello():
+    print("Hello World !")
+
+Hello()
